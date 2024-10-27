1f119f72ba414bd7c1dc0bc6dc57d93ca78fbbb4
catrival <58177583+catrival@users.noreply.github.com>
2024-10-27 13:29:09 -0500

    Initial structure to log in

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,47 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import { AuthProvider, useAuth } from './components/login/AuthContext'
 import Footer from "./components/footer";
 import Header from "./components/header";
 import StudentsList from "./components/students/StudentsList";
 import StudentsFormCreate from "./components/students/StudentsFormCreate";
 import StudentsFormEdit from "./components/students/StudentsFormEdit";
-
 import BooksList from "./components/books/BooksList";
 import BooksFormCreate from "./components/books/BooksFormCreate";
 import BooksFormEdit from "./components/books/BooksFormEdit";
-
 import LoansList from "./components/loans/LoansList";
 import LoansFormCreate from "./components/loans/LoansFormCreate";
 import LoansFormEdit from "./components/loans/LoansFormEdit";
+import LoginAccess from "./components/login/LoginAccess";
 
+
+const PrivateRoute = ({ element }) => {
+  const { isAuthenticated } = useAuth();
+  return isAuthenticated ? element : <Navigate to="/" />;
+};
 
 function App() {
-
+  const { isAuthenticated } = useAuth();
 
   return (
-    <>      
+    <AuthProvider>
       <BrowserRouter>
-        <Header />
+        {isAuthenticated && <Header />}
         <Routes>
-
-          <Route path="/student" element={<StudentsList />} />
-          <Route path="/student/:id" element={<StudentsFormEdit />} />
-          <Route path="/create-student" element={<StudentsFormCreate />} />
-
-          <Route path="/books" element={<BooksList />} />
-          <Route path="/book/:id" element={<BooksFormEdit />} />
-          <Route path="/create-book" element={<BooksFormCreate />} />
-
-          <Route path="/loans" element={<LoansList />} />
-          <Route path="/loan/:id" element={<LoansFormEdit />} />
-          <Route path="/create-loan" element={<LoansFormCreate />} />
-
+          <Route path="/" element={<LoginAccess />} />
+          <Route path="/student" element={<PrivateRoute element={<StudentsList />} />} />
+          <Route path="/student/:id" element={<PrivateRoute element={<StudentsFormEdit />} />} />
+          <Route path="/create-student" element={<PrivateRoute element={<StudentsFormCreate />} />} />
+          <Route path="/books" element={<PrivateRoute element={<BooksList />} />} />
+          <Route path="/book/:id" element={<PrivateRoute element={<BooksFormEdit />} />} />
+          <Route path="/create-book" element={<PrivateRoute element={<BooksFormCreate />} />} />
+          <Route path="/loans" element={<PrivateRoute element={<LoansList />} />} />
+          <Route path="/loan/:id" element={<PrivateRoute element={<LoansFormEdit />} />} />
+          <Route path="/create-loan" element={<PrivateRoute element={<LoansFormCreate />} />} />
         </Routes>
         <Footer />
       </BrowserRouter>
-    </>
+    </AuthProvider>
   );
 }
 
---
+++ src/components/login/LoginAccess.jsx
@@ -0,0 +1,57 @@
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import LoginForm from "./LoginForm";
+import axios from "axios";
+import jwt_decode from 'jwt-decode';
+
+export default function LoginAccess() {
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:5085/Auth/login",
+        {
+          email: e.target.email.value,
+          password: e.target.password.value,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const token = response.data.token;
+      if (token) {
+        localStorage.setItem('token', token);
+        const decoded = jwt_decode(token);
+        const userInfo = jwt_decode(token);
+        localStorage.setItem('user', JSON.stringify(userInfo));
+      }
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Inicio de sesión exitoso",
+        showConfirmButton: false,
+        timer: 1500,
+      }).then(() => {
+        navigate("/books");
+      });
+    } catch (error) {
+      Swal.fire({
+        position: "center",
+        icon: "error",
+        title: "Error al iniciar sesión "+ error,
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+  };
+  return (
+    <LoginForm
+      props={{
+        handleSubmit: handleSubmit,
+      }}
+    />
+  );
+}
