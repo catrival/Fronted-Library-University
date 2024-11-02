70c79ae892898a565fa27827d0014b820268ec00
catrival <58177583+catrival@users.noreply.github.com>
2024-11-02 12:54:33 -0500

    Do not show nav options and show centered notifications

--- src/components/loans/LoansForm.jsx
+++ src/components/loans/LoansForm.jsx
@@ -1,40 +1,55 @@
 /** Componente reutilizable para Crear y Actualizar un Usuario */
 export default function LoansForm({ props }) {
-    const { handleSubmit, loan, students, books } = props
-    return (
-        <div className="max-w-md w-full mx-auto px-5 py-5 h-screen">
-            <form onSubmit={handleSubmit} className="shadow-md rounded pt-6 pb-10 mb-4 px-10 mt-3">
-                <div className="mb-4">
-                    <label for="underline_select" class="sr-only">Underline select</label>
-                    <select id="underline_select"  class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2
+  const { handleSubmit, loan, students, books } = props;
+  return (
+    <div className="max-w-md w-full mx-auto px-5 py-5 h-screen">
+      <form
+        onSubmit={handleSubmit}
+        className="shadow-md rounded pt-6 pb-10 mb-4 px-10 mt-3"
+      >
+        <div className="mb-4">
+          <label for="underline_select" class="sr-only">
+            Underline select
+          </label>
+          <select
+            id="underline_select"
+            class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2
                      border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0
-                      focus:border-gray-200 peer" defaultValue={loan?.book_Id} name="book_id">
-                        <option selected>Selecciona el libro</option>
-                        {
-                            books.map(book=>(
-                                <option value={book.id}>{book.name}</option>
-                            ))
-                        }
-                    </select>
-                </div>
-                <div className="mb-4">
-                    <label for="underline_select" class="sr-only">Underline select</label>
-                    <select id="underline_select" class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2
-                     border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0
-                      focus:border-gray-200 peer" defaultValue={loan?.student_Id} name="student_id">
-                        <option selected>Selecciona el estudiante</option>
-                        {
-                            students.map(student=>(
-                                <option value={student.id}>{student.name}</option>
-                            ))
-                        }
-                    </select>
-                </div>
-
-                <div className="flex justify-center">
-                    <button className="bg-blue-500 hover:bg-blue-700 rounded text-blue-50 font-bold py-2 px-4">Save</button>
-                </div>
-            </form>
+                      focus:border-gray-200 peer"
+            defaultValue={loan?.book_Id}
+            name="book_id"
+          >
+            <option selected>Selecciona el libro</option>
+            {books.map((book) => (
+              <option value={book.id}>{book.name}</option>
+            ))}
+          </select>
         </div>
-    );
+        <div className="mb-4">
+          <label for="underline_select" class="sr-only">
+            Underline select
+          </label>
+          <select
+            id="underline_select"
+            class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2
+                     border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0
+                      focus:border-gray-200 peer"
+            defaultValue={loan?.student_Id}
+            name="student_id"
+          >
+            <option selected>Selecciona el estudiante</option>
+            {students.map((student) => (
+              <option value={student.id}>{student.name}</option>
+            ))}
+          </select>
+        </div>
+
+        <div className="flex justify-center">
+          <button className="bg-blue-500 hover:bg-blue-700 rounded text-blue-50 font-bold py-2 px-4">
+            Save
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
